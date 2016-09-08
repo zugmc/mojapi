85ff28746907fb632fb70841f43e94250c7718bb
gozug <gozug@zugmc.com>
2016-09-08 22:54:12 +0100

    Add get profiles call

--- mojapi/api.py
+++ mojapi/api.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import time
 
@@ -11,4 +13,18 @@ def get_uuid(username, unix_timestamp=None):
         unix_timestamp = int(time.time())
     return requests.get(
         'https://api.mojang.com/users/profiles/minecraft/{}?at={}'.format(username, unix_timestamp)
+    ).json()
+
+
+def get_usernames(uuid):
+    return requests.get('https://api.mojang.com/user/profiles/{}/names'.format(uuid)).json()
+
+
+def get_profiles(*usernames):
+    return requests.post(
+        url='https://api.mojang.com/profiles/minecraft',
+        headers={
+            b'Content-Type': b'application/json'
+        },
+        data=json.dumps(list(usernames))
     ).json()
