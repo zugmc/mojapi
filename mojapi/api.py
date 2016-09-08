b86a7b8ea70760d9914ab59adb7a5810c5a0f37f
gozug <gozug@zugmc.com>
2016-09-08 22:45:24 +0100

    Add get uuid call

--- mojapi/api.py
+++ mojapi/api.py
@@ -1,5 +1,14 @@
 import requests
+import time
 
 
 def get_statuses():
     return requests.get('https://status.mojang.com/check/').json()
+
+
+def get_uuid(username, unix_timestamp=None):
+    if unix_timestamp is None:
+        unix_timestamp = int(time.time())
+    return requests.get(
+        'https://api.mojang.com/users/profiles/minecraft/{}?at={}'.format(username, unix_timestamp)
+    ).json()
